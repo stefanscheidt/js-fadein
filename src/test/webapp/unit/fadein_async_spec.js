39ee3965f1d98e862d36d6de2ad8bd0aa0a02622
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-09-05 15:37:36 +0200

    added fadein_aync_spec

---
+++ src/test/webapp/unit/fadein_async_spec.js
@@ -0,0 +1,23 @@
+describe("fadein", function () {
+
+    describe("for execute", function() {
+
+        it("should asynchronously set the opacity to 1 in the end", function () {
+            var opacitySpy = spyOn(util, 'opacity');
+            function lastOpacityValue() {
+                return opacitySpy.mostRecentCall.args[1];
+            }
+            runs(function() {
+                fadein.execute('dummyField');
+            });
+            waitsFor(function() {
+                return lastOpacityValue()===1;
+            });
+            runs(function() {
+                // some other expectations
+                expect(lastOpacityValue()).toEqual(1);
+            });
+        });
+    });
+
+});
