ec11dba6185278f30a1f8c3a5e822e23bf433cc0
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2012-04-18 11:12:29 +0200

    use jasmine.Clock

--- src/test/webapp/unit/fadein_spec.js
+++ src/test/webapp/unit/fadein_spec.js
@@ -1,29 +1,27 @@
 describe("fadein", function () {
 
-    describe("for execute", function() {
+    describe("for execute", function () {
 
-        var field, setTimeoutSpy, opacitySpy;
+        var field;
 
         beforeEach(function () {
-            field = "test";
-            setTimeoutSpy = spyOn(window, 'setTimeout').andCallFake(function(callback) {
-                callback();
-            });
-            opacitySpy = spyOn(util, 'opacity');
+            field = document.createElement("div");
+            jasmine.Clock.useMock();
+            spyOn(util, 'opacity');
         });
 
         it("should set the opacity to 1 in the end", function () {
             fadein.execute(field);
-            expect(opacitySpy.mostRecentCall.args[1]).toEqual(1);
+            jasmine.Clock.tick(1000);
+            expect(util.opacity.mostRecentCall.args[1]).toEqual(1);
         });
 
         it("should call opacity with range from 0 to 1 in steps of 0.01", function () {
             fadein.execute(field);
-            expect(opacitySpy.callCount).toEqual(100);
             for (var i = 0; i < 100; i++) {
-                expect(opacitySpy.argsForCall[i][0]).toBe(field);
-                expect(opacitySpy.argsForCall[i][1]).toEqual((i + 1) / 100);
-
+                expect(util.opacity.mostRecentCall.args[0]).toBe(field);
+                expect(util.opacity.mostRecentCall.args[1]).toBe((i + 1) / 100);
+                jasmine.Clock.tick(10);
             }
         });
     });
